39f0ac2a35ce7979d59ad9cbb824f889c517791f
yii-meow <yiksoonchong2002@gmail.com>
2023-04-26 19:47:28 +0800

    INIT

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+    const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+
+    const vote = (id) => {
+        dispatch(voteAnecdote(id))
+    }
+
+    return (
+        anecdotes.map(anecdote =>
+            <div key={anecdote.id}>
+                <div>
+                    {anecdote.content}
+                </div>
+
+                <div>
+                    has {anecdote.votes}
+                    <button onClick={() => vote(anecdote.id)}>vote</button>
+                </div>
+            </div>
+        )
+    )
+}
+
+export default AnecdoteList
